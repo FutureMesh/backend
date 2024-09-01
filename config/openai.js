83b4acb6730dbe1e2157f4288ca7ff6bcc889cb0
axbuglak <alexeybuglak24@gmail.com>
2024-09-02 00:19:02 +0200

    linted

--- config/openai.js
+++ config/openai.js
@@ -7,7 +7,7 @@ module.exports = {
     'It does not necessarily have to be a good future, you should remain as objective as possible and tell the truth, even if it is negative.' +
     'You should look no more than 5 years ahead. Your answer should be as informative as possible and, no more than 500 words, respectively, without introductions, retellings, etc.' +
     'Just one paragraph of text without categories or additional information, like summery of his life. ' +
-    'It\'is also possible that the information about the person will not be in English, so you should give an answer in the language in which the information about the person will be presented.',
+    "It'is also possible that the information about the person will not be in English, so you should give an answer in the language in which the information about the person will be presented.",
   chat: {
     model: 'gpt-4o-mini',
   },
