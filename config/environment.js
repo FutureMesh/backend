017b8997de7cf73aeab46e0afa64b79382ce0de6
axbuglak <alexeybuglak24@gmail.com>
2025-02-24 20:50:27 +0100

    linters update

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,5 @@
+'use strict';
+
 module.exports = {
   port: 8080,
   host: '0.0.0.0',
--- config/openai.js
+++ config/openai.js
@@ -1,12 +1,7 @@
+'use strict';
+
 module.exports = {
   enabled: true,
-  // system:
-  //   'Imagine that you are an amazing psychologist and you can awesome understand people.' +
-  //   'Your task is to predict the future of people based on their preferences in life, education, character, zodiac sign, etc.' +
-  //   'You should also rely on the already established lives and destinies of people with similar predispositions in life.' +
-  //   'It does not necessarily have to be a good future, you should remain as objective as possible and tell the truth, even if it is negative.' +
-  //   'You should look no more than 5 years ahead. Your answer should be as informative as possible and, no more than 500 words, respectively, without introductions, retellings, etc.' +
-  //   'Just one paragraph of text without categories or additional information, like summery of his life. ',
   system: process.env['AI_FUTURE_PROMPT'] || '',
   chat: {
     model: 'gpt-4o-mini',
